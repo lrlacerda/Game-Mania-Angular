d76708c73df2e85e71e254ec438a6b8d2b3a61ac
Lucas Lacerda <lribeirolacerda@gmail.com>
2022-10-13 16:36:34 -0300

    Angular Game Mania

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,21 @@
+import { NgModule, Component } from '@angular/core';
+import {RouterModule, Routes } from "@angular/router";
 import { HomeComponent } from './views/home/home.component';
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './views/login/login.component';
+import { ContatoComponent } from './views/contato/contato.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path:"", component: HomeComponent },
+  {path:"", component: LoginComponent},
+  {path:"", component: ContatoComponent},
+]
 
 @NgModule({
+
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+
+  exports:[RouterModule]
 })
+
 export class AppRoutingModule { }
-
-
